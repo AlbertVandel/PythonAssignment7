644449dfaa07960ee8043b57c305eb085ff2cd1c
Albert Vandel <albertvandel@yahoo.dk>
2019-03-31 14:11:16 +0200

    part 1 should be finished now

--- dba_module.py
+++ dba_module.py
@@ -18,3 +18,21 @@ def search_recent(phrase):
     browser.implicitly_wait(1)
     b = browser.find_element_by_xpath("//span[contains(text(),'Seneste 24 timer')]")
     browser.execute_script("arguments[0].click();",b)
+
+def search_cph(phrase):
+    search(phrase)
+    browser.implicitly_wait(1)
+    browser.find_element_by_xpath("//a[contains(text(),'København og omegn')]").click()
+    return browser.page_source
+
+def search_cph_recent(phrase):
+    search(phrase)
+    browser.implicitly_wait(3)
+    browser.find_element_by_xpath("//a[contains(text(),'København og omegn')]").click()
+    browser.implicitly_wait(3)
+    a = browser.find_element_by_xpath("//h4[contains(text(),'Oprettet')]")    
+    browser.execute_script("arguments[0].click();",a)
+    browser.implicitly_wait(1)
+    b = browser.find_element_by_xpath("//span[contains(text(),'Seneste 24 timer')]")
+    browser.execute_script("arguments[0].click();",b)  
+    return browser.page_source
